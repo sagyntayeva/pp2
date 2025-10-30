17b835bfa14e493d3796477d0ea62c6d795b0c17
Sagyntayeva Ranida <r_sagyntayeva@kbtu.kz>
2025-10-31 01:34:26 +0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,42 @@
+import pygame 
+import time
+import math
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Mickey clock")
+
+leftarm = pygame.image.load('C:\\Users\\ranid\\OneDrive\\Desktop\\pp2\\second.png').convert_alpha()
+rightarm = pygame.image.load('C:\\Users\\ranid\\OneDrive\\Desktop\\pp2\\minute.png').convert_alpha()
+mainclock = pygame.transform.scale(pygame.image.load('C:\\Users\\ranid\\OneDrive\\Desktop\\pp2\\base_micky.jpg').convert_alpha(), (800, 600))
+
+
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+
+    current_time = time.localtime()
+    minute = current_time.tm_min
+    second = current_time.tm_sec
+    
+    minute_angle = minute * 6 + 45
+    second_angle = second * 6 
+    
+    screen.blit(mainclock, (0,0))
+    
+    rotated_rightarm = pygame.transform.rotate(pygame.transform.scale(rightarm, (800, 600)), -minute_angle)
+    rightarmrect = rotated_rightarm.get_rect(center=(800 // 2, 600 // 2))
+    screen.blit(rotated_rightarm, rightarmrect)
+    
+    rotated_leftarm = pygame.transform.rotate(pygame.transform.scale(leftarm, (40.95, 682.5)), -second_angle)
+    leftarmrect = rotated_leftarm.get_rect(center=(800 // 2, 600 // 2))
+    screen.blit(rotated_leftarm, leftarmrect)
+    
+    pygame.display.flip()
+    clock.tick(60) 
+    
+pygame.quit()
